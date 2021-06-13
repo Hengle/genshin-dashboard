4fb0da4a03d35694348574802cca54ece2c2bd63
Koding Dev <kodingdev@pm.me>
2021-06-13 15:41:56 +1000

    :lipstick: Updated character card

--- src/pages/database/characters.tsx
+++ src/pages/database/characters.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { InferGetStaticPropsType } from "next";
 import { fetchCharacters } from "@/api/database/avatar/character";
-import { Card, Col, InputNumber, Row } from "antd";
+import { Button, Card, Col, InputNumber, Row, Typography } from "antd";
 import { CharacterCard, characters } from "@/assets/database/characters";
 import { calculateStat } from "@/util/avatar";
+import { StarFilled } from "@ant-design/icons";
+import { StatType } from "@/types/database";
+import _ from "lodash";
 
 type CharacterCardProps = {
   character: CharacterCard;
@@ -17,17 +20,34 @@ const CharacterComponent = ({
   ascension,
 }: CharacterCardProps) => (
   <div>
-    <Row>
-      <Col>
-        <img alt="" src={character.assets.card} width="30%" />
-        <h1>{character.data.name}</h1>
+    <Row gutter={[8, 0]}>
+      <Col span={6}>
+        <Card cover={<img alt="" src={character.assets.card} />}>
+          <Card.Meta
+            title={character.data.name}
+            description={
+              <>
+                {Array.from(Array(character.data.stars), (v) => (
+                  <StarFilled key={v} style={{ color: "gold" }} />
+                ))}
+              </>
+            }
+          />
+        </Card>
       </Col>
-      <Col>
-        <h1>Base Stats</h1>
-        <p>HP: {calculateStat(character.data, "HP", level, ascension)}</p>
-        <p>ATK: {calculateStat(character.data, "ATK", level, ascension)}</p>
-        <p>DEF: {calculateStat(character.data, "DEF", level, ascension)}</p>
-        <p>STA: {calculateStat(character.data, "STA", level, ascension)}</p>
+      <Col flex="auto">
+        <Card>
+          <Card.Meta title="Base Stats" />
+          <Row>
+            {(["HP", "ATK", "DEF", "STA"] as StatType[]).map((v) => (
+              <Col span={4} key={v}>
+                <Typography.Text>
+                  {v}: {calculateStat(character.data, v, level, ascension)}
+                </Typography.Text>
+              </Col>
+            ))}
+          </Row>
+        </Card>
       </Col>
     </Row>
   </div>
@@ -42,30 +62,41 @@ const Characters = ({
   );
 
   const [level, setLevel] = useState(1);
-  const [ascension, setAscension] = useState(1);
+  const [ascended, setAscended] = useState(false);
+  const ascension = _.last(
+    Object.values(user.data.ascension.levels.levels).filter(
+      (value) => value.rewards.unlockLevel <= level,
+    ),
+  );
 
   return (
     <div>
-      <h1>Database: Characters</h1>
-      <Card>
+      <Typography.Title level={3}>Database: Characters</Typography.Title>
+      <Typography.Text>Level</Typography.Text>
+      <div>
         <InputNumber
           min={1}
           max={90}
           defaultValue={level}
           onChange={(level) => setLevel(level ?? 1)}
         />
-        <InputNumber
-          min={1}
-          max={Object.keys(user.data.ascension.levels.levels).length}
-          defaultValue={ascension}
-          onChange={(level) => setAscension(level ?? 1)}
-        />
-        <CharacterComponent
-          character={user}
-          ascension={ascension}
-          level={level}
-        />
-      </Card>
+        <Button
+          onClick={() => setAscended(!ascended)}
+          type={ascended ? "primary" : "default"}
+          disabled={ascension?.rewards.unlockLevel !== level}
+        >
+          Ascended
+        </Button>
+      </div>
+      <br />
+      <CharacterComponent
+        character={user}
+        ascension={
+          (ascension?.level ?? 0) +
+          (ascended || ascension?.rewards.unlockLevel !== level ? 1 : 0)
+        }
+        level={level}
+      />
     </div>
   );
 };
